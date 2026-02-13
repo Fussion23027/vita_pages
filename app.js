ca6ae17fa8fbeebeff211f8cf629b140e3aaf639
Pedro Gerardo <150979861+Fussion23027@users.noreply.github.com>
2026-02-13 16:29:45 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
-// Ready for future Guest / Admin logic
-console.log("AnimReddit fully loaded");
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+console.log(" AnimReddit fully loaded");
+
+const supabaseUrl = 'https://pqcgecgvfastavdxlesc.supabase.co';
+const supabaseKey = 'sb_publishable_FMaKOlreN3hkZfvMiun47g_cOzNrYuM';
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+async function registrarVisita() {
+  console.log(" Intentando registrar visita...");
+
+  const sessionId = crypto.randomUUID();
+
+  const { data, error } = await supabase
+    .from('usuarios')
+    .insert([
+      { session_id: sessionId }
+    ]);
+
+  if (error) {
+    console.error(" Error insertando:", error);
+  } else {
+    console.log(" Usuario registrado correctamente:", data);
+  }
+}
+
+registrarVisita();
